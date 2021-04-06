48678b703a58fbdac9d32656c03ff5d82f438131
gstar612 <gstar612@gmail.com>
2021-04-06 11:10:07 -0700

    Create createUser.js

---
+++ Adobe/createUser.js
@@ -0,0 +1,38 @@
+const axios = require('axios')
+let secrets
+
+module.exports = function(input, callback, error) {
+  secrets = input.secrets
+  let webhookRequest = input.request
+  let data = [{
+    "user": webhookRequest.body.email,
+    "requestID": "create_user",
+    "do": [{
+      "createFederatedID": {
+        "email": webhookRequest.body.email,
+        "firstname": webhookRequest.body.firstName,
+        "lastname": webhookRequest.body.lastName,
+        "option": "ignoreIfAlreadyExists"
+      }
+    }]
+  }]
+  
+  generateAccessToken().then(token => {
+    webhookRequest.headers.Authorization = `Bearer ${token}`
+    webhookRequest.body = data
+    callback(webhookRequest)
+  })
+}
+
+function generateAccessToken() {
+  return new Promise(resolve => {
+    let generateTokenRequest = {
+      method: 'POST',
+      url: `https://ims-na1.adobelogin.com/ims/exchange/jwt?client_id=${secrets.client_id}&client_secret=${secrets.client_secret}&jwt_token=${secrets.jwt_token}`
+    }
+    
+    axios(generateTokenRequest).then(response => {
+      resolve(response.data.access_token)
+    })
+  })
+}
