0653bf2d96f44cd943675c20357130e8a3a96c15
gstar612 <gstar612@gmail.com>
2021-04-06 11:16:30 -0700

    Create removeFromLicenseGroup.js

---
+++ Adobe/removeFromLicenseGroup.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+let secrets
+
+module.exports = function(input, callback, error) {
+  secrets = input.secrets
+  let webhookRequest = input.request
+  let data = [{
+    "user": webhookRequest.body.email,
+    "requestID": "remove_license",
+    "do": [{
+      "remove": {
+        "group": [webhookRequest.body.license]
+      }
+    }]
+  }]
+  
+  generateAccessToken().then(token => {
+    webhookRequest.headers.Authorization = `Bearer ${token}`
+    webhookRequest.body = data
+    callback(webhookRequest)
+  })
+}
+
+function generateAccessToken() {
+  return new Promise(resolve => {
+    let generateTokenRequest = {
+      method: 'POST',
+      url: `https://ims-na1.adobelogin.com/ims/exchange/jwt?client_id=${secrets.client_id}&client_secret=${secrets.client_secret}&jwt_token=${secrets.jwt_token}`
+    }
+    
+    axios(generateTokenRequest).then(response => {
+      resolve(response.data.access_token)
+    })
+  })
+}
