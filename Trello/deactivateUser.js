f2e01b607cb312df0cf9211a39e152781a7993d7
gstar612 <gstar612@gmail.com>
2021-04-06 11:19:03 -0700

    Create deactivateUser.js

---
+++ Trello/deactivateUser.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+let secrets
+
+module.exports = function(input, callback, error) {
+  secrets = input.secrets
+  let webhookRequest = input.request
+  let data = {
+    "schemas": ['urn:ietf:params:scim:api:messages:2.0:PatchOp'],
+    "Operations": [{"op": "replace", "value": {"active": false}}]
+  }
+  
+  getUserId(webhookRequest.body.email).then(userId => {
+    webhookRequest.url += userId
+    webhookRequest.body = data
+    callback(webhookRequest)
+  })
+}
+
+function getUserId(email) {
+  return new Promise(resolve => {
+    let getUserIdRequest = {
+      method: "GET",
+      url: `https://trello.com/scim/v2/users?filter=emails.value eq "${email}"`,
+      headers: {
+        Authorization: `Bearer ${secrets.trelloToken}`
+      }
+    }
+    
+    axios(getUserIdRequest).then(response => {
+      let userId = response.data.Resources[0].id
+      resolve(userId)
+    })
+  })
+}
